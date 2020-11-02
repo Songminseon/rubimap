290fd7e6417429f12bb34f18c993af16a63f62f3
Songminseon <smsun0329@naver.com>
2020-11-02 21:46:07 +0900

    data_add

---
+++ rubimap/public/data/data.js
@@ -0,0 +1,19 @@
+var data = [
+    {
+        name:"루비샵",
+        open_time:"9:00~17:00",
+        phone : "010-5029-2283",
+        lat: 37.660434,
+        lng: 126.98704,
+        detail_description:"루비네 샵에 오신걸 환영합니다."
+    },
+    {
+        name:"하티샵",
+        open_time:"9:00~17:00",
+        phone : "010-5029-2283",
+        lat: 37.650434,
+        lng: 126.97704,
+        detail_description:"하비네 샵에 오신걸 환영합니다."
+    }
+
+];
