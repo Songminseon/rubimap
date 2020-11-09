6aca543131e1a82c8464592b7d2f193e5eaff348
Songminseon <smsun0329@naver.com>
2020-11-09 20:36:20 +0900

    mark_shop

--- rubimap/public/data/data.js
+++ rubimap/public/data/data.js
@@ -1,19 +1,56 @@
-var data = [
-    {
-        name:"루비샵",
-        open_time:"9:00~17:00",
-        phone : "010-5029-2283",
-        lat: 37.660434,
-        lng: 126.98704,
-        detail_description:"루비네 샵에 오신걸 환영합니다."
-    },
-    {
-        name:"하티샵",
-        open_time:"9:00~17:00",
-        phone : "010-5029-2283",
-        lat: 37.650434,
-        lng: 126.97704,
-        detail_description:"하비네 샵에 오신걸 환영합니다."
-    }
+var data = [{
+    name: "라라펫샵",
+    category: "펫샵",
+    open_time: "10:00~19:00",
+    phone: "010-5029-2283",
+    lat: 37.542350,
+    lng: 127.066830,
+    detail_description: "라라펫을 만나면 예뻐져요. 건강해져요.세련되져요.매너가 좋아져요"
+},
+{
+    name: "메인동물메디컬센터",
+    category: "동물병원",
+    open_time: "9:00~21:00",
+    phone: "02-979-8575",
+    lat: 37.540376,
+    lng: 127.067581,
+    detail_description: "2015년 6월에 문을 연 메인동물메디컬센터는 우수한 의료진과 전문성을 갖춘 인력으로 의료 서비스를 제공하고 있습니다."
+},
+{
+    name: "펫시터 리치네집",
+    category: "펫호텔",
+    open_time: "00:00~24:00",
+    phone: "010-7142-7739",
+    lat: 37.539398,
+    lng: 127.069024,
+    detail_description: "반려견에게 줄 수 있는 최고의 선물! 행복한 리치네집을 선물해주세요"
+},
+{
+    name: "바르다펫",
+    category: "펫미용",
+    open_time: "10:00~17:00",
+    phone: "010-4452-2553",
+    lat: 37.541287,
+    lng: 127.069764,
+    detail_description: "애견 미용 전문점 바르다펫 입니다."
+},
+{
+    name: "위드유어펫",
+    category: "펫용품점",
+    open_time: "9:00~17:00",
+    phone: "010-55841-7510",
+    lat: 37.538905,
+    lng: 127.066041,
+    detail_description: "한국 공식 수입원 위드유어펫입ㄴ디ㅏ."
+},
+{
+    name: "하티샵",
+    category: "펫호텔",
+    open_time: "9:00~17:00",
+    phone: "010-5029-2283",
+    lat: 37.541406,
+    lng: 127.068444,
+    detail_description: "하비네 샵에 오신걸 환영합니다."
+},
 
 ];
