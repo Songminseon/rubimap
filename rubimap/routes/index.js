5417d42e3e1b3d7566a56220d57d40e4b395c60c
Songminseon <smsun0329@naver.com>
2020-11-16 16:02:18 +0900

    pleaseFrontGoGo

--- rubimap/routes/index.js
+++ rubimap/routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/petshop', function(req,res){
+  res.render('petshop', {title:'petshop'})
+})
+
+router.get('/pethair', function(req,res){
+  res.render('pethair', {title:'pethair'})
+})
+
+router.get('/pethospital', function(req,res){
+  res.render('pethospital', {title:'pethospital'})
+})
+
+router.get('/petthing', function(req,res){
+  res.render('petthing', {title:'petthing'})
+})
+
+router.get('/pethotel', function(req,res){
+  res.render('pethotel', {title:'pethotel'})
+})
+
+
+
+
 module.exports = router;
